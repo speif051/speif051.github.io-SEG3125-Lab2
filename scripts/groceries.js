6bad013c48f8b0ed9cd4d412bc4ae1f53aee7479
speif051 <31969206+speif051@users.noreply.github.com>
2020-06-10 15:24:02 -0400

    Update groceries.js

--- scripts/groceries.js
+++ scripts/groceries.js
@@ -86,26 +86,26 @@ var products = [
 
 function restrictListProducts(prods, restriction) {
 	let product_names = [];
-	let product_prices = [];
+	//let product_prices = [];
 	for (let i=0; i<prods.length; i+=1) {
 		if ((restriction == "Vegetarian") && (prods[i].vegetarian == true)){
 			product_names.push(prods[i].name);
-			product_prices.push(prods[i].price);
+			//product_prices.push(prods[i].price);
 		}
 		else if ((restriction == "GlutenFree") && (prods[i].glutenFree == true)){
 			product_names.push(prods[i].name);
-			product_prices.push(prods[i].price);
+			//product_prices.push(prods[i].price);
 		}
 		else if ((restriction == "Organic") && (prods[i].organic == true)){
 			product_names.push(prods[i].name);
-			product_prices.push(prods[i].price);
+			//product_prices.push(prods[i].price);
 		}
 		else if (restriction == "None"){
 			product_names.push(prods[i].name);
-			product_prices.push(prods[i].price);
+			//product_prices.push(prods[i].price);
 		}
 	}
-	return [product_names, product_prices];
+	return product_names;
 }
 
 // Calculate the total price of items, with received parameter being a list of products
