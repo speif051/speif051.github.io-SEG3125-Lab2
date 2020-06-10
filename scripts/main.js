6bd5c77c81a75c8a34893aef5f4f29715c1b98f8
speif051 <31969206+speif051@users.noreply.github.com>
2020-06-10 15:15:03 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -34,7 +34,7 @@ function populateListProductChoices(slct1, slct2) {
     s2.innerHTML = "";
 		
 	// obtain a reduced list of products based on restrictions
-    var optionArray = restrictListProducts(products, s1.value);
+    var optionArray, priceArray = restrictListProducts(products, s1.value);
 
 	// for each item in the array, create a checkbox element, each containing information such as:
 	// <input type="checkbox" name="product" value="Bread">
